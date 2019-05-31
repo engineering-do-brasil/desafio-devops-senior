47ad475c0eb53b6df75632e5ec3d3c238fb8e683
Italo Rafael Oliveira Santos <italo.santos@engdb.com.br>
2019-05-31 11:56:48 -0300

    Bookmanager APP

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# desafio-devops-senior
+# Bem vindo ao desafio DevOps da Engineering do Brasil.
+
+Neste desafio queremos validar seus conhecimentos como DevOps, para tanto temos uma app em Python3, esse app é um CRUD de uma base de livros. Este app foi desenvolvido usando o SQLite e precisamos incluir uma base de dados, você decide qual será a base usada, o importante é que a applicação funcione. 
+Esperamos que ao final você nos gere uma imagem Docker contendo o código dessa app funcional.
+
+Faça um fork desse projeto, commit todas suas mudanças em seu projeto e nos envie o link do seu fork, para avaliação.
+
+## Entregáveis:
+Como entregáveis esperamos ao menos um arquivo Dockerfile e um README.md explicando como gerar sua imagem e como rodar um container usando sua imagem e um arquivo de deploy, seja ele para deploy em Docker Swarm ou Kubernetes.
+
+## Você ganha pontos extras se:
+Se fizer um script que ao ser executado, uma imagem seja criada e os containers criados para um teste local.
+Se fizer melhorias no código, como melhrorias entendendemos desde refatorar o código, até criar uma interface web mais bonita.
+Se fizer monitoração
---
+++ bookmanager.py
@@ -0,0 +1,61 @@
+import os
+
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    books = None
+    if request.form:
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+    books = Book.query.all()
+    return render_template("home.html", books=books)
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
